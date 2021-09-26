bc0bba6cb99785c5d6d31605cf6e529cbda06460
unknown <leo-crux1997@hotmail.com>
2021-09-26 18:29:12 +0400

    in process of adding user search

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,25 +1,81 @@
 // JavaScript source code
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native'
+
+import firebase from 'firebase'
+require ("firebase/firestore")
 
 import { connect } from 'react-redux'
 
 function Profile(props) {
 
+    const [userPost, setUserPosts] = useState([]);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const { currentUser, posts } = props;
+        console.log({ currentUser, posts })
+
+        if (props.route.params.uid === firebase.auth().currentUser.uid) {
+            setUser(currentUser)
+            setUserPosts(posts)
+        }
+        else {
+            firebase.firestore()
+                .collection("users")
+                .doc(props.route.params.uid)
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.exists) {
+                        //console.log(snapshot.data())
+                        setUser(snapshot.data());
+                         
+
+                    }
+                    else {
+                        console.log('does not exit!')
+                    }
+                })
+
+            firebase.firestore()
+                .collection("posts")
+                .doc(props.route.params.uid)
+                .collection("userPosts")
+                .orderBy("creation", "asc")
+                .get()
+                .then((snapshot) => {
+                    let posts = snapshot.docs.map(doc => {
+                        const data = doc.data();
+                        const id = doc.id;
+                        return { id, ...data }
+
+                    })
+                    //console.log(posts)
+                    setUserPosts(posts)
+                })
+        }
+
+    } , [props.route.params.uid])
+
+    if (user === null) {
+        return <View/>
+
+    }
+
     const { currentUser, posts } = props;
     console.log({currentUser, posts})
     return (
         <View style={ styles.container}>
-            <View style={ styles.containerInfo}>
-                <Text> name: { currentUser.name } </Text>
-                <Text> email: { currentUser.email } </Text>
+            <View style={styles.containerInfo}>
+                <Text> name: {user.name} </Text>
+                <Text> email: {user.email} </Text>
             </View>
 
             <View style={styles.containerGallery}>
                 <FlatList
                     numColumns={3}
                     horizontal={false}
-                    data={posts}
+                    data={userPost}
                     renderItem={({ item }) => (
                         <View style={ styles.containerImage}>
                         <Image
