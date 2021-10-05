2530c66edc32f6036ac2b3fc3f13c1a481797dd6
unknown <leo-crux1997@hotmail.com>
2021-10-05 18:23:41 +0400

    added working keyinfo screen

--- components/main/Keyinfo.js
+++ components/main/Keyinfo.js
@@ -1,98 +1,47 @@
-// JavaScript source code
-import React, { useEffect, useState } from 'react'
-import { View, Text, Image, FlatList, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, Flatlist, Button, TextInput } from 'react-native'
 
 import firebase from 'firebase'
 require("firebase/firestore")
 
-import { connect } from 'react-redux'
+export default function Keyinfo(props) {
 
-function Keyinfo(props) {
+    const [keydetails, setKeydetails] = useState([])
+    const [keyHistory, setKeyHistory] = useState([])
+    const [keyId, setKeyId] = useState("")
+    const [text, setText] = useState("")
 
-    const [userKeys, setUserKeys] = useState(null);
-    const [user, setUser] = useState(null);
+    //console.log(props.route.params.keyId)
+    //console.log(props.route.params.uid)
+
 
     useEffect(() => {
-        const { currentUser, keyinfodetails } = props;
-        //console.log({ currentUser, posts })
 
-        setUser(currentUser)
-
-
-        
-            
+        if (props.route.params.keyId !== keyId) {
             firebase.firestore()
-                .collection("keycollection")
-                .doc(firebase.auth().currentUser.uid)
-                .collection("keylist")
+                .collection('keycollection')
                 .doc(props.route.params.uid)
+                .collection('keylist')
+                .doc(props.route.params.keyId)
                 .get()
                 .then((snapshot) => {
-                    if (snapshot.exists) {
-
-                        setUserKeys(snapshot.data());
-                        //console.log(snapshot.data())
-
-
-                    }
-                    else {
-                        console.log('does not exit!')
-                    }
+                    setKeydetails(snapshot.data())
                 })
-                
+            setKeyId(props.route.params.keyId)
 
-       
-    }, [props.route.params.uid])
+        }
 
+    }, [props.route.params.keyId])
 
-    if (user === null) {
-        return <View />
-
-    }
-
-
-    console.log(props.route.params.uid + ' ' + firebase.auth().currentUser.uid + ' keyinfo screen ')
-    console.log(userKeys + " userkeys state")
+    //console.log(keydetails)
 
     return (
-        <View style={styles.container}>
-            <View style={styles.containerInfo}>
-                <Text> name:  </Text>
-                <Text> email:  </Text>
+
+        <View>
+            <Text> keyinfo screen </Text>
+            <Text> key name: { keydetails.keyname} </Text>
+            <Text> key location: {keydetails.keylocation} </Text>
+
             </View>
-        </View>
-    )
+        )
 }
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 40
-    },
-    containerInfo: {
-        margin: 20,
-    },
-    containerGallery: {
-        flex: 1,
-    },
-    image: {
-        flex: 1,
-        aspectRatio: 1 / 1
-    },
-    containerImage: {
-        flex: 1 / 3
-    },
-    containerKeylist: {
-        flex: 1,
-        alignItems: "center",
-    }
-})
-
-const mapStateToProps = (store) => ({
-    currentUser: store.userState.currentUser,
-    keyinfodetails: store.userState.keyinfodetails
-})
-
-export default connect(mapStateToProps, null)(Keyinfo)
---
+++ components/main/Profile (2).js
@@ -0,0 +1,157 @@
+// JavaScript source code
+import React, { useEffect, useState } from 'react'
+import { View, Text, Image, FlatList, StyleSheet, Button, TouchableOpacity } from 'react-native'
+
+import firebase from 'firebase'
+require ("firebase/firestore")
+
+import { connect } from 'react-redux'
+    
+function Profile(props) {
+
+    const [userPost, setUserPosts] = useState([]);
+    const [userKeys, setUserKeys ] = useState([]);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const { currentUser, posts, keyinfo} = props;
+        //console.log({ currentUser, posts })
+
+        if (props.route.params.uid === firebase.auth().currentUser.uid) {
+            setUser(currentUser)
+            //setUserPosts(posts)
+            setUserKeys(keyinfo)
+
+            //console.log(keyinfo)
+        }
+        else {
+            firebase.firestore()
+                .collection("users")
+                .doc(props.route.params.uid)
+                .get()
+                .then((snapshot) => {
+                    if (snapshot.exists) {
+
+                        setUser(snapshot.data());
+                        //console.log(snapshot.data())
+                         
+
+                    }
+                    else {
+                        console.log('does not exit!')
+                    }
+                })
+
+            /*
+            firebase.firestore()
+                .collection("posts")
+                .doc(props.route.params.uid)
+                .collection("userPosts")
+                .orderBy("creation", "asc")
+                .get()
+                .then((snapshot) => {
+                    let posts = snapshot.docs.map(doc => {
+                        const data = doc.data();
+                        const id = doc.id;
+                        return { id, ...data }
+                    })
+                    setUserPosts(posts)
+                })
+                */
+            firebase.firestore()
+                .collection("keycollection")
+                .doc(props.route.params.uid)
+                .collection("keylist")
+                .orderBy("creation", "asc")
+                .get()
+                .then((snapshot) => {
+                    let keyinfo = snapshot.docs.map(doc => {
+                        const data = doc.data();
+                        const id = doc.id;
+                        return { id, ...data }
+                    })
+                    setUserKeys(keyinfo)
+                })
+
+        }
+
+    }, [props.route.params.uid])
+
+    const onLogout = () => {
+        firebase.auth().signOut();
+    }
+
+    if (user === null) {
+        return <View/>
+
+    }
+    //console.log({currentUser, posts})
+
+    console.log(props.route.params.uid + ' profile screen ')
+
+    return (
+        <View style={ styles.container}>
+            <View style={styles.containerInfo}>
+                <Text> name: {user.name} </Text>
+                <Text> email: {user.email} </Text>
+
+                <Button title='logout' onPress={() =>onLogout()}/>
+            </View>
+
+            <View style={styles.containerGallery}>
+
+                <FlatList
+                    numColumns={1}
+                    horizontal={false}
+                    data={userKeys}
+                    renderItem={({ item }) => (
+                        <View style={styles.containerKeylist}>
+                            <TouchableOpacity
+                                onPress={() => props.navigation.navigate("Keyinfo", { uid: item.id })}>
+                                <Text> {item.keyname} {item.keylocation} {item.id} </Text>
+                            </TouchableOpacity>
+
+                        </View>
+
+                    )}
+                />
+
+            </View>
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 40
+    },
+    containerInfo: {
+        margin: 20,
+    },
+    containerGallery: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1/1
+    },
+    containerImage: {
+        flex: 1/3
+    },
+    containerKeylist: {
+        flex: 1,
+        alignItems: "center",
+        margin: 10,
+    }
+})
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    posts: store.userState.posts,
+    keyinfo: store.userState.keyinfo
+})
+
+export default connect(mapStateToProps, null)(Profile)
