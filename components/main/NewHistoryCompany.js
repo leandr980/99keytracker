76b7d61a63a65110048dd6681a8efa8275455c35
leandro <leo-crux1997@hotmail.com>
2022-02-24 17:59:33 +0400

    added radio buttons for keyinfo screen and created base sceen for landlord,company,agent,other screens

---
+++ components/main/NewHistoryCompany.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function NewHistoryCompany() {
+
+
+    return (
+        <View>
+            <Text> NewHistoryCompany page </Text>
+        </View>
+        )
+}
---
+++ components/main/NewHistoryOther.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function NewHistoryOther() {
+
+
+    return (
+        <View>
+            <Text> NewHistoryOther page </Text>
+        </View>
+        )
+}
