2d8af5a7ce5802b59df4d378d739dae9f537db61
leandro <leo-crux1997@hotmail.com>
2022-05-12 18:49:40 +0400

    added packages for notification and device

--- components/main/Leadinfo.js
+++ components/main/Leadinfo.js
@@ -1,18 +1,31 @@
-import React, { useState, useEffect, cloneElement } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { View, Text, FlatList, StyleSheet, ImageBackground, TextInput, SafeAreaView, Image} from 'react-native'
 import { Card, FAB, IconButton, Divider, Chip, DataTable, Searchbar, Caption, Title, Button, Switch, Banner} from 'react-native-paper'
 import { format } from 'date-fns'
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import DateTimePicker from '@react-native-community/datetimepicker'
 
-
 import firebase from 'firebase'
-import { Icon } from 'react-native-elements'
 require("firebase/firestore")
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
 
 export default function Leadinfo(props) {
 
     const creation = firebase.firestore.FieldValue.serverTimestamp()
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
 
     const [leadinfo, setleadinfo] = useState([])
     const [leadinfodate, setleadinfodate] = useState([])
@@ -62,11 +75,23 @@ export default function Leadinfo(props) {
                             }
                         }
                     })
+
+                    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+                    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+                      setNotification(notification);
+                    });
+                
+                    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+                      console.log(response);
+                    });
                     
 
                 return () => {
                     subscribe()
                     subscribe2()
+                    Notifications.removeNotificationSubscription(notificationListener.current);
+                    Notifications.removeNotificationSubscription(responseListener.current);
                 }
             }, [])
     
@@ -233,6 +258,37 @@ export default function Leadinfo(props) {
         </View>
         )
 }
+
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
 const styles = StyleSheet.create({
     container: {
